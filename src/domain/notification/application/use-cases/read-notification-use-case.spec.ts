f74fe28864c93308ff7e705c62d8349bd109ca96
Felipe Barbosa do Nascimento <dfelipebdn@gmail.com>
2024-01-22 07:44:50 -0300

    0744d220224

--- src/domain/notification/application/use-cases/read-notification-use-case.spec.ts
+++ src/domain/notification/application/use-cases/read-notification-use-case.spec.ts
@@ -2,18 +2,21 @@ import { makeNotification } from 'test/factories/make-notification'
 import { InMemoryNotificationRepository } from 'test/repositories/in-memory-notification-repository'
 import { expect } from 'vitest'
 
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { NotAllowedError } from '@/core/errors/not-allowed-error'
+
 import { ReadNotificationUseCase } from './read-notification-use-case'
 
 let inMemoryNotificationRepository: InMemoryNotificationRepository
 let sut: ReadNotificationUseCase
 
-describe('Create Category', () => {
+describe('Read Notification', () => {
   beforeEach(() => {
     inMemoryNotificationRepository = new InMemoryNotificationRepository()
     sut = new ReadNotificationUseCase(inMemoryNotificationRepository)
   })
 
-  it('should be able to create a new category', async () => {
+  it('should be able to read a notification', async () => {
     const notification = makeNotification()
     inMemoryNotificationRepository.items.push(notification)
 
@@ -23,6 +26,22 @@ describe('Create Category', () => {
     })
 
     expect(result.isRight()).toBeTruthy()
-    expect(inMemoryNotificationRepository.items[0].readAt).toBeTruthy()
+    expect(inMemoryNotificationRepository.items[0].readAt).toEqual(
+      expect.any(Date),
+    )
+  })
+  it('should be able to read a notification from another user', async () => {
+    const notification = makeNotification({
+      recipientId: new UniqueEntityId('recipient-01'),
+    })
+    inMemoryNotificationRepository.items.push(notification)
+
+    const result = await sut.execute({
+      notificationId: notification.id.toValue(),
+      recipientId: 'recipient-02',
+    })
+
+    expect(result.isLeft()).toBeTruthy()
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
